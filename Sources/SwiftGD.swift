572182cad57a18591f78d59aef28263f6fe98186
Hannes Sverrisson <sverrisson@ruv.is>
2017-05-16 18:53:59 +0000

    Removed unneccessary code

--- Sources/SwiftGD.swift
+++ Sources/SwiftGD.swift
@@ -8,12 +8,7 @@
 
 import Foundation
 
-// In case you were wondering: it's a class rather than a struct because we need
-// deinit to free the internal GD pointer, and that's only available to classes.
 public class Image {
-	public enum FlipMode {
-		case horizontal, vertical, both
-	}
 
 	private var internalImage: gdImagePtr
 
@@ -149,110 +144,9 @@ public class Image {
 		return Image(gdImage: output)
 	}
 
-	public func fill(from: Point, color: Color) {
-		let internalColor = gdImageColorAllocateAlpha(internalImage, Int32(color.redComponent * 255.0), Int32(color.greenComponent * 255.0), Int32(color.blueComponent * 255.0), 127 - Int32(color.alphaComponent * 127.0))
-		defer { gdImageColorDeallocate(internalImage, internalColor) }
-
-		gdImageFill(internalImage, Int32(from.x), Int32(from.y), internalColor)
-	}
-
-	public func drawLine(from: Point, to: Point, color: Color) {
-		let internalColor = gdImageColorAllocateAlpha(internalImage, Int32(color.redComponent * 255.0), Int32(color.greenComponent * 255.0), Int32(color.blueComponent * 255.0), 127 - Int32(color.alphaComponent * 127.0))
-		defer { gdImageColorDeallocate(internalImage, internalColor) }
-
-		gdImageLine(internalImage, Int32(from.x), Int32(from.y), Int32(to.x), Int32(to.y), internalColor)
-	}
-
-	public func set(pixel: Point, to color: Color) {
-		let internalColor = gdImageColorAllocateAlpha(internalImage, Int32(color.redComponent * 255.0), Int32(color.greenComponent * 255.0), Int32(color.blueComponent * 255.0), 127 - Int32(color.alphaComponent * 127.0))
-		defer { gdImageColorDeallocate(internalImage, internalColor) }
-
-		gdImageSetPixel(internalImage, Int32(pixel.x), Int32(pixel.y), internalColor)
-	}
-
-	public func get(pixel: Point) -> Color {
-		let color = gdImageGetTrueColorPixel(internalImage, Int32(pixel.x), Int32(pixel.y))
-
-		let a = Double((color >> 24) & 0xFF)
-		let r = Double((color >> 16) & 0xFF)
-		let g = Double((color >> 8) & 0xFF)
-		let b = Double(color & 0xFF)
-
-		return Color(red: r / 255, green: g / 255, blue: b / 255, alpha: 1 - (a / 127))
-	}
-
-	public func strokeEllipse(center: Point, size: Size, color: Color) {
-		let internalColor = gdImageColorAllocateAlpha(internalImage, Int32(color.redComponent * 255.0), Int32(color.greenComponent * 255.0), Int32(color.blueComponent * 255.0), 127 - Int32(color.alphaComponent * 127.0))
-		defer { gdImageColorDeallocate(internalImage, internalColor) }
-
-		gdImageEllipse(internalImage, Int32(center.x), Int32(center.y), Int32(size.width), Int32(size.height), internalColor)
-	}
-
-	public func fillEllipse(center: Point, size: Size, color: Color) {
-		let internalColor = gdImageColorAllocateAlpha(internalImage, Int32(color.redComponent * 255.0), Int32(color.greenComponent * 255.0), Int32(color.blueComponent * 255.0), 127 - Int32(color.alphaComponent * 127.0))
-		defer { gdImageColorDeallocate(internalImage, internalColor) }
-
-		gdImageFilledEllipse(internalImage, Int32(center.x), Int32(center.y), Int32(size.width), Int32(size.height), internalColor)
-	}
-
-	public func strokeRectangle(topLeft: Point, bottomRight: Point, color: Color) {
-		let internalColor = gdImageColorAllocateAlpha(internalImage, Int32(color.redComponent * 255.0), Int32(color.greenComponent * 255.0), Int32(color.blueComponent * 255.0), 127 - Int32(color.alphaComponent * 127.0))
-		defer { gdImageColorDeallocate(internalImage, internalColor) }
-
-		gdImageRectangle(internalImage, Int32(topLeft.x), Int32(topLeft.y), Int32(bottomRight.x), Int32(bottomRight.y), internalColor)
-	}
-
-	public func fillRectangle(topLeft: Point, bottomRight: Point, color: Color) {
-		let internalColor = gdImageColorAllocateAlpha(internalImage, Int32(color.redComponent * 255.0), Int32(color.greenComponent * 255.0), Int32(color.blueComponent * 255.0), 127 - Int32(color.alphaComponent * 127.0))
-		defer { gdImageColorDeallocate(internalImage, internalColor) }
-
-		gdImageFilledRectangle(internalImage, Int32(topLeft.x), Int32(topLeft.y), Int32(bottomRight.x), Int32(bottomRight.y), internalColor)
-	}
-
-	public func flip(_ mode: FlipMode) {
-		switch mode {
-		case .horizontal:
-			gdImageFlipHorizontal(internalImage)
-		case .vertical:
-			gdImageFlipVertical(internalImage)
-		case .both:
-			gdImageFlipBoth(internalImage)
-		}
-	}
-
-	public func pixelate(blockSize: Int) {
-		gdImagePixelate(internalImage, Int32(blockSize), GD_PIXELATE_AVERAGE.rawValue)
-	}
-
-	public func blur(radius: Int) {
-		if let result = gdImageCopyGaussianBlurred(internalImage, Int32(radius), -1) {
-			gdImageDestroy(internalImage)
-			internalImage = result
-		}
-	}
-
-	public func colorize(using color: Color) {
-		gdImageColor(internalImage, Int32(color.redComponent * 255), Int32(color.greenComponent * 255), Int32(color.blueComponent * 255), 127 - Int32(color.alphaComponent * 127.0))
-	}
-
-	public func desaturate() {
-		gdImageGrayScale(internalImage)
-	}
-
 	deinit {
 		// always destroy our internal image resource
 		gdImageDestroy(internalImage)
-	}
-}
-
-
-public struct Point {
-	public var x: Int
-	public var y: Int
-
-	public init(x: Int, y: Int) {
-		self.x = x
-		self.y = y
 	}
 }
 
@@ -264,24 +158,4 @@ public struct Size {
 		self.width = width
 		self.height = height
 	}
-}
-
-public class Color {
-	public var redComponent: Double
-	public var greenComponent: Double
-	public var blueComponent: Double
-	public var alphaComponent: Double
-
-	public init(red: Double, green: Double, blue: Double, alpha: Double) {
-		redComponent = red
-		greenComponent = green
-		blueComponent = blue
-		alphaComponent = alpha
-	}
-
-	public static let red = Color(red: 1, green: 0, blue: 0, alpha: 1)
-	public static let green = Color(red: 0, green: 1, blue: 0, alpha: 1)
-	public static let blue = Color(red: 0, green: 0, blue: 1, alpha: 1)
-	public static let black = Color(red: 0, green: 0, blue: 0, alpha: 1)
-	public static let white = Color(red: 1, green: 1, blue: 1, alpha: 1)
 }
